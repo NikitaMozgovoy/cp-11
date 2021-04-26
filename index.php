d2fa50f713f273c541fbcece1303a87c8a5e827d
mozgovoy <60607081+mozgovoy@users.noreply.github.com>
2021-04-26 15:25:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,23 +17,23 @@
     switch ($op) {
     case "add":
         $res = $n1 + $n2;
-        echo json_encode($res);
+        echo $res;
         break;
     case "substract":
         $res = $n1 - $n2;
-        echo json_encode($res);
+        echo $res;
         break;
     case "multiply":
         $res = $n1 * $n2;
-        echo json_encode($res);
+        echo $res;
         break;
     case "divide":
         $res = $n1 / $n2;
-        echo json_encode($res);
+        echo $res;
         break;
     case "exponentiate":
         $res = $n1 ** $n2;
-        echo json_encode($res);
+        echo $res;
         break;
     }
     }
